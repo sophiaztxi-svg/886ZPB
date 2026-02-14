52725407f56f04b7778232de835eda1748da5993
sophiaztxi-svg <sophia.zt.xi@gmail.com>
2026-02-14 13:54:01 -0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,88 @@
 #include "main.h"
+#include "lemlib/chassis/trackingWheel.hpp"
 #include "pros/misc.h"
 #include "pros/motor_group.hpp"
-// Motor intake(7);
+#include <cstdint>
+#include "lemlib/api.hpp"
+#include "pros/motors.h"
+#include "pros/rtos.h"
+
 
 // Negative turn is left
 //Positive turn is right
 
 
-Motor lIntake(7); // lower
-Motor mfIntake (-14); // middle/transition
-Motor mbIntake (20);
-Motor uIntake(11); // upper
-MotorGroup intakes({7,12,15,-14});
-Imu inertial(19);
+Motor Intake(-3); // lower
+Motor uIntake(6); // upper
 
-Motor leftFront = (3);
-Motor rightFront = (6);
-MotorGroup left_mg({-1, -2, -3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-MotorGroup right_mg({4, 5, 6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+Imu inertial(5);
+Rotation rotation_sensor(15);//placeholder port
 
-adi::Pneumatics piston('a', false); //
-adi::Pneumatics matchLoad('b', false);
+// vertical tracking wheel
+lemlib::TrackingWheel vertical_tracking_wheel(&rotation_sensor, lemlib::Omniwheel::NEW_325, -2.5);
+
+Motor leftFront = (10);
+Motor rightFront = (-4);
+MotorGroup left_mg({10, -14, -21});    // Creates a motor group with forwards ports 1 & 3 and reversed port 
+MotorGroup right_mg({-4, 13, 16});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+
+
+lemlib::Drivetrain drivetrain(&left_mg, // left motor group
+                              &right_mg, // right motor group
+                              10, // 10 inch track width
+                              lemlib::Omniwheel::NEW_325, // using new 4" omnis
+                              600 * 0.75, // drivetrain rpm is 360
+                              2 // horizontal drift is 2 (for now)
+);
+
+adi::Pneumatics matchLoad('e', false);
 adi::Pneumatics descore('c', false);
+adi::Pneumatics transition('a', false);
+
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            nullptr, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &inertial // inertial sensor
+);
+
+lemlib::ControllerSettings lateral_controller(0.25, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              0.5, // derivative gain (kD)
+                                              2, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(1.6, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              6, // derivative gain (kD)
+                                              2, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors // odometry sensors
+);
 /**
+
  * A callback function for LLEMU's center button.
  *
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
+
+
 void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
@@ -46,9 +101,23 @@ void on_center_button() {
  */
 void initialize() {
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
 	inertial.reset(true);
 	pros::lcd::register_btn1_cb(on_center_button);
+
+	chassis.calibrate(); // calibrate sensors
+    // print position to brain screen
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
+    
+	
 }
 
 /**
@@ -96,7 +165,7 @@ int speedLimit(int speed){
 
  int conversionTool (float inches){
 	
-	double wheelDiameter = 2.75,
+	double wheelDiameter = 3.25,
 		Pi = 3.1415,
 		gearRatio = 4.0/3.0, // wheel gear / motor gear
 		ticksPerRev = 300.0;
@@ -104,8 +173,7 @@ int speedLimit(int speed){
 		return inches / (Pi * wheelDiameter) * ticksPerRev * gearRatio;
  }
 
-
-void moveEncoder(int target, int time){ // forward and backward
+ void moveEncoder(int target, int time){ // forward and backward
 	double kp = 1.6;
 	double pastError;
 	double derivative;
@@ -122,14 +190,15 @@ void moveEncoder(int target, int time){ // forward and backward
 	left_mg.move(0);// stop if no command is given
 	right_mg.move(0);
 }
+
 void driveStraight(int distance, int time, int smallTimeOut){ // autocorrect/ forceful correct when driving in autonomous
 	int ticks = conversionTool(distance);
-	double kp = 0.6;//0.15;//double kp = 0.05; for auton
-	double rkp = 1; // 0.1
+	double kp = 0.25;// 0.6 for others
+	double rkp = 0.1; // 0.1
 	inertial.tare_rotation();
 	double pastError;
 	double derivative;
-	double kd = 2.4; 
+	double kd = 0.5; // 2.4 others
 
 	int STF = millis(); // start time of function
 	right_mg.tare_position();
@@ -137,7 +206,8 @@ void driveStraight(int distance, int time, int smallTimeOut){ // autocorrect/ fo
 	double error = ticks - right_mg.get_position();
 	double tTime = millis();
 
-	while(millis() - STF < time && millis() - tTime < smallTimeOut){//millis() - tTime < 500 , millis() - STF < time
+	while( millis() - STF < time && millis() - tTime < smallTimeOut){//millis() - tTime < 500 , millis() - STF < time || millis() - STF < time && millis() - tTime < smallTimeOut
+		
 		error = ticks - right_mg.get_position();
 		derivative = pastError- error;
 		rError = inertial.get_rotation();
@@ -158,11 +228,11 @@ void driveStraight(int distance, int time, int smallTimeOut){ // autocorrect/ fo
 
 void turnInertial(int degrees, int time, int smallTimeOut){ // turning, measured by the inertial sensor so it can be precise: counter clockwise --> negative/ clockwise --> positive
 	inertial.tare_rotation();
-	double kp = 2;//1.6;// auton 1.1
+	double kp = 1.6;//1.6;// auton 1.1
 	double error = degrees - inertial.get_rotation();
 	double pastError;
 	double derivative;
-	double kd = 3;
+	double kd = 6;
 	int STF = millis();
 	double tTime = millis();
 
@@ -183,72 +253,287 @@ void turnInertial(int degrees, int time, int smallTimeOut){ // turning, measured
 	right_mg.move(0);
 }
 
+void hold(int time, int speed) {
+	leftFront.move(speed);
+	rightFront.move(speed);
+	delay(time);
+	leftFront.move(0);
+	rightFront.move(0);
+}
+
 void autonomous() {
+
+	// Comment out other auton codes when doing each
+	// use project.pros to name each code, assign to different slots
+	// 1 tile length = 24 inches
+
 	//right corners
-		// matchLoad.extend();
-		// driveStraight(32, 1000, 600);
-		// turnInertial(93,600,400);
-		// lIntake.move(127);
-		// mfIntake.move(127);
-		// mbIntake.move(127);
-		// uIntake.move(-20);
-		// driveStraight(10,800,400);
-		// turnInertial(10,400,300);
-		// turnInertial(-10, 400, 300);
-		// delay(1200);
-		// driveStraight(-16,1000, 400);
-		// matchLoad.retract();
-		// turnInertial(170,800,400);
-		// driveStraight(7, 1000, 400);
-		// turnInertial(12,800,400);
-		// driveStraight(3, 1000, 400);
-		// uIntake.move(127);
-		// delay(1800);
-
-			// piston.extend();
-			// driveStraight(-10,1000, 500);
-			// driveStraight(14,800,400);
-
-		// driveStraight(-10,1000, 500);
-		// turnInertial(-60,600,400);
-		// driveStraight(25,800,500);
-
-
-
-	// left corners
-	//check config
-		matchLoad.extend();
+		
+		 
+		// ---------------------------------below!!!!
+		descore.extend();
 		driveStraight(33, 1000, 600);
-		turnInertial(-90,600,400);
-		lIntake.move(127);
-		mfIntake.move(127);
-		mbIntake.move(127);
-		uIntake.move(-20);
-		driveStraight(12,800,400);
-		turnInertial(-15,400,300);
-		turnInertial(15, 400, 300);
-		delay(1200);
-		driveStraight(-16,1000, 400);
+		delay(700);
+		turnInertial(92,800,500);
+		delay(300);
+		driveStraight(-7,800,400); //backup
+		matchLoad.extend();
+		delay(300);
+		Intake.move(127); //run Intake
+		uIntake.move(0); //store
+		driveStraight(14,800,400);
+		hold(1200,45); //load from loader
+		driveStraight(-29,1200, 800);
 		matchLoad.retract();
-		// turnInertial(-170,800,400);
-		// driveStraight(7, 1000, 400);
-		// turnInertial(-12,800,400);
-		// driveStraight(3, 1000, 400);
-		// uIntake.move(127);
-		// delay(1800);
+		uIntake.move(127);
+		delay(2600); // score in high goal
 
-		// driveStraight(-10,1000, 500);
-		// turnInertial(60,600,400);
-		// matchLoad.extend();
-		// driveStraight(25,800,500);
+		uIntake.move(0);
 
+
+
+		driveStraight(16,1000, 500);
+		turnInertial(130,600,400); // turn 
+		driveStraight(25,1000,800);
+		delay(1000); // load blocks on the ground
+		driveStraight(15,800,500);
+		uIntake.move(-127); // score on low goal
+		Intake.move(-127);
+		
+		delay(2500);
+	
+
+//------------------------------------------------------------------------------
+
+
+// 	// left corners
+// 	//check config-- below!!!!
+
+// 		// ---------------------------------below!!!!
+// 		descore.extend();
+// 		driveStraight(33, 1000, 600);
+// 		delay(700);
+// 		turnInertial(-92,800,500);
+// 		delay(300);
+// 		driveStraight(-7,800,400); //backup
+// 		matchLoad.extend();
+// 		delay(300);
+// 		Intake.move(127); //run Intake
+// 		uIntake.move(0); //store
+// 		driveStraight(14,800,400);
+// 		hold(1200,45); //load from loader
+// 		driveStraight(-29,1200, 800);
+// 		matchLoad.retract();
+// 		uIntake.move(127);
+// 		delay(2600); // score in high goal
+
+// 		uIntake.move(0);
+
+// 		driveStraight(16,1000, 500);
+// 		turnInertial(-130,600,400); // turn 
+// 		driveStraight(27,1000,800);
+// 		delay(1000); // load blocks on the ground
+// 		turnInertial(180,800,500); //turn
+// 		driveStraight(-15,800,500);
+// 		uIntake.move(-127); // score on low goal
+// 		Intake.move(-127);
+		
+// 		delay(2500);
+	
+
+// 	//-------------------------------------------------------------------
+	
+// 	//Skills
+
+// 		//set 1: left 2 loaders, 1 long goal , clear & park
+// 		descore.extend();
+// 		driveStraight(33, 1000, 600);
+// 		delay(700);
+// 		turnInertial(-92,800,500);
+// 		delay(300);
+// 		driveStraight(-7,800,400); //backup
+// 		matchLoad.extend();
+// 		delay(300);
+// 		Intake.move(127); //run Intake
+// 		uIntake.move(0); //store
+// 		driveStraight(14,800,400);
+// 		hold(1200,45); //load from loader
+// 		driveStraight(-10,1000, 400);
+// 		matchLoad.retract();
+// 		Intake.move(0);
+
+// 		uIntake.move(0);
+// 		turnInertial(135,600,400);
+// 		driveStraight(19,600,300);
+// 		turnInertial(35,500,250);
+// 		driveStraight(72,4000,3000); // drive to other side
+// 		turnInertial(90,400,300);
+// 		driveStraight(-15,600,500);// hard reset
+// 		driveStraight(16,800,600);
+// 		delay(1000);
+
+		
+// 		delay(1000);
+// 		turnInertial(-90,400,300);
+// 		driveStraight(-20,1000,800);
+// 		Intake.move(127);
+// 		uIntake.move(127);
+// 		delay(2600); // score in high goal 1- 1
+// 		matchLoad.extend();
+// 		delay(300);
+// 		uIntake.move(0);
+
+// 		driveStraight(28,1000,800);
+// 		hold(1000,15); //load from loader 2
+// 		driveStraight(-30,1200, 800);
+// 		matchLoad.retract();
+// 		Intake.move(127);
+// 		uIntake.move(127);
+// 		delay(2600); // score in high goal 1-2
+// 		Intake.move(0);
+// 		uIntake.move(0);
+
+// 		driveStraight(-8,1000, 500);// drive back
+// 		turnInertial(90,400,300);
+// 		driveStraight(20, 3000,2500);
+// 		turnInertial(75,400,300);
+// 		delay(1000);
+// 		driveStraight(90, 3000,2500); //come back
+// 		turnInertial(-90,400,300);
+// 		uIntake.move(127); // clear parking
+// 		driveStraight(30,800,700);
+// 		driveStraight(-16,500,400);// backup & park
+
+
+
+// 		//--------------------------------------------------------------------------
+// 		//Set 2: Full: 4 loaders, 2 goals, clear and park
+
+// 		descore.extend();
+// 		driveStraight(33, 1000, 600);
+// 		delay(700);
+// 		turnInertial(-92,800,500);
+// 		delay(300);
+// 		driveStraight(-7,800,400); //backup
+// 		matchLoad.extend();
+// 		delay(300);
+// 		Intake.move(127); //run Intake
+// 		uIntake.move(0); //store
+// 		driveStraight(14,800,400);
+// 		hold(1200,45); //load from loader
+// 		driveStraight(-10,1000, 400);
+// 		matchLoad.retract();
+// 		Intake.move(0);
+
+// 		uIntake.move(0);
+// 		turnInertial(135,600,400);
+// 		driveStraight(19,600,300);
+// 		turnInertial(35,500,250);
+// 		driveStraight(72,4000,3000); // drive to other side
+// 		turnInertial(90,400,300);
+// 		driveStraight(-15,600,500);// hard reset
+// 		driveStraight(16,800,600);
+// 		delay(1000);
+
+		
+// 		delay(1000);
+// 		turnInertial(-90,400,300);
+// 		driveStraight(-20,1000,800);
+// 		Intake.move(127);
+// 		uIntake.move(127);
+// 		delay(2600); // score in high goal 1- 1
+// 		matchLoad.extend();
+// 		delay(300);
+// 		uIntake.move(0);
+
+// 		driveStraight(28,1000,800);
+// 		hold(1000,15); //load from loader 2
+// 		driveStraight(-30,1200, 800);
+// 		matchLoad.retract();
+// 		Intake.move(127);
+// 		uIntake.move(127);
+// 		delay(2600); // score in high goal 1-2
+// 		Intake.move(0);
+// 		uIntake.move(0);
+// 		driveStraight(18,800,600);
+// 		turnInertial(90,600,500);
+// 		driveStraight(80,1600,1400); // go to loader 3
+// 		turnInertial(-90,600,500);	
+
+// 		matchLoad.extend();
+// 		Intake.move(127);
+// 		driveStraight(28,1000,800);
+// 		hold(1000,15); //load from loader 3
+// 		driveStraight(-27,1200, 800);
+// 		matchLoad.retract();	
+
+// 		Intake.move(0);
+// 		uIntake.move(0);
+// 		turnInertial(135,600,400);
+// 		driveStraight(19,600,300);
+// 		turnInertial(35,500,250);
+// 		//descore.extend();
+// 		driveStraight(72,4000,3000); // drive to other side
+// 		turnInertial(90,400,300);
+// 		driveStraight(-15,600,500);// hard reset
+// 		driveStraight(16,800,600);
+// 		delay(1000);
+
+// 		turnInertial(-90,400,300);
+// 		driveStraight(-20,1000,800);
+// 		Intake.move(127);
+// 		uIntake.move(127);
+// 		delay(2600); // score in high goal 2- 1
+// 		matchLoad.extend();
+// 		delay(300);
+// 		uIntake.move(0);
+
+// 		driveStraight(28,1000,800);
+// 		hold(1000,15); //load from loader 4
+// 		driveStraight(-28,800,700);
+// 		uIntake.move(127);
+// 		delay(2600); // score in high goal 2- 2
+// 		matchLoad.retract();
+// 		uIntake.move(0);
+
+// 		driveStraight(20,800,700);
+// 		turnInertial(-90,600,400);
+// 		driveStraight(-20,800,700);
+// 		turnInertial(90,600,400);
+// 		driveStraight(10,800,700);
+// 		turnInertial(90,600,400);
+
+// 		uIntake.move(127); // clear parking
+// 		driveStraight(30,800,700);
+// 		driveStraight(-16,500,400);// backup & park
 
 		
 
+// //----------------------------------------------
+// //backup & park
 
+// 		uIntake.move(127); // clear parking
+// 		Intake.move(127);
+// 		driveStraight(30,800,700);
+// 		driveStraight(-16,500,400);// backup & park
+
+
+
+//-------------------------------------------------------------------------
+
+// chassis.turnToHeading(90, ); //turning- degrees
+// chassis.turnToPoint(float x, float y, int timeout); // move with coordinates
+// chassis.setPose(0,0,0); // set current position as 0
+
+// chassis.swingToHeading(float theta, int timeout);// swing to face x degrees
+// chassis.swingToPoint(float x, float y, int timeout); // swing to face the point 
+
+// chassis.moveToPoint(float x, float y, int timeout);// move to (x,y)- straight
+// chassis.moveToPose(float x, float y, float theta, int timeout)//  move to (x,y)- with turn
 
 
 }
+
 
 	
 
@@ -268,72 +553,60 @@ void autonomous() {
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	bool clampState = 0;
+	pros::lcd::set_text(1, "Test!");
 	
+	uIntake.set_brake_mode(E_MOTOR_BRAKE_HOLD);
 	while (true) {
 
 		// Arcade control scheme
-		int turn = master.get_analog(ANALOG_RIGHT_X);    // Gets amount forward/backward from left joystick
-		int dir = master.get_analog(ANALOG_LEFT_Y);  // Gets the turn left/right from right joystick
+		int turn = master.get_analog(ANALOG_RIGHT_X) * 0.85;    // Gets amount forward/backward from left joystick
+		int dir = master.get_analog(ANALOG_LEFT_Y)* 0.85;  // Gets the turn left/right from right joystick
 		left_mg.move(dir + turn);                      // Sets left motor voltage
 		right_mg.move(dir - turn);                     // Sets right motor voltage
 
 
 
+		//
+			if (master.get_digital(DIGITAL_R1)) { // long goal store // S L1
+				Intake.move(127);// up
+				uIntake.move(0);// up
+				// transition.extend();
+			}
+			else if (master.get_digital(DIGITAL_R2)) { // outtake 
+				Intake.move(-127); // down
+				uIntake.move(-127); // down
+			
+			}
 
+			else if (master.get_digital (DIGITAL_L1)) { //scoring long goal
+				Intake.move(127);// up
+				uIntake.move(127);// up
+			}
+			else if (master.get_digital(DIGITAL_L2)) { //middle
+				transition.retract();
+				Intake.move (127);
+				uIntake.move(-127);
+			}
 
-		if (master.get_digital(DIGITAL_R1)){
-			mfIntake.move(127);//127 is the max speed
-			mbIntake.move(127);
-			uIntake.move(127);
-			lIntake.move(127);
-			if(master.get_digital(DIGITAL_LEFT)) {
+			else {
+				Intake.move(0);
 				uIntake.move(0);
 			}
-			if(master.get_digital(DIGITAL_UP)) {
-				mfIntake.move(-127);
-				mbIntake.move(-127);
-
-			}
-		}
-		else if (master.get_digital(DIGITAL_R2)){
-			mfIntake.move(-90);//-127
-			mbIntake.move(-90);
-			uIntake.move(-90);
-			lIntake.move(-90);
-		}
-		else {
-			mfIntake.move (0);
-			mbIntake.move(0);
-			uIntake.move(0);
-			lIntake.move(0);
-		}
-
-		if (master.get_digital_new_press(DIGITAL_L2)){
-			piston.toggle();
-		}
 		
-		if (master.get_digital(DIGITAL_L1)) {
-			lIntake.move(127);
-			mfIntake.move(127);
-			mbIntake.move(127);
-			uIntake.move(-20);
-		}
 
-		if (master.get_digital(DIGITAL_RIGHT)){
-			mfIntake.move(-127);
-			mbIntake.move(127);
-			uIntake.move(127);
-			lIntake.move(127);
-		}
-
-		if (master.get_digital_new_press(DIGITAL_A)){
+		if (master.get_digital_new_press(DIGITAL_Y)){ // matchload //S X
 			matchLoad.toggle();
 		}
 
-		if (master.get_digital_new_press(DIGITAL_Y)){
-			descore.toggle();
+		if (master.get_digital_new_press(DIGITAL_X)){ // descore // SY
+			descore.toggle(); 
 		}
 
+		if (master.get_digital_new_press(DIGITAL_B)){ // transition in L2 
+			transition.toggle();
+		}
+
+		
 				delay(20); 
 		                 // Run for 20 ms then update
 	}
